fa6ac713265920dd202574d5167e859eb6365e4f
maximilianozurita <maxi.zurita.seb@gmail.com>
2021-05-09 22:20:51 -0300

    SFML prueba

---
+++ recursos/prueba smfl/tp 2 proy/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#include <cstring>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1080,700),"Ventana");
+
+    system ("pause");
+    return 0;
+
+}
